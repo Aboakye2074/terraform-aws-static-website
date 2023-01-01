19a9b92a16414dc9077815cc55d609d4104e349d
Bahaa Noah <bahaa.noah93@gmail.com>
2023-01-01 21:46:46 +0400

    feat: added acm

---
+++ acm.tf
@@ -0,0 +1,41 @@
+//cloudfront only works with certificates in us-east-1
+provider "aws" {
+  alias   = "virginia"
+  region  = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.domainName
+  validation_method = "DNS"
+
+  tags                      = var.tags
+  subject_alternative_names = [var.wwwDomainName]
+  lifecycle {
+    create_before_destroy = true
+  }
+  provider = aws.virginia
+
+}
+
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.primary.zone_id
+
+  depends_on = [
+    aws_acm_certificate.this,
+    aws_route53_zone.primary
+  ]
+}
---
+++ route53.tf
@@ -0,0 +1,36 @@
+resource "aws_route53_zone" "primary" {
+  name = var.domainName
+  tags = var.tags
+}
+
+resource "aws_route53_record" "bahaanoah-com" {
+  name    = var.domainName
+  type    = "A"
+  zone_id = aws_route53_zone.primary.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.bahaanoah-com-s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.bahaanoah-com-s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [
+    aws_cloudfront_distribution.bahaanoah-com-s3_distribution
+  ]
+}
+
+resource "aws_route53_record" "www-bahaanoah-com" {
+  name    = var.wwwDomainName
+  type    = "A"
+  zone_id = aws_route53_zone.primary.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.www-bahaanoah-com-s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www-bahaanoah-com-s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [
+    aws_cloudfront_distribution.www-bahaanoah-com-s3_distribution
+  ]
+}
