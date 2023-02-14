a53dc6c86cac7b401821e3fd3e5ed50cf281b2fd
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2023-02-13 20:37:58 -0500

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -3,27 +3,27 @@ resource "aws_route53_zone" "primary" {
   tags = var.tags
 }
 
-resource "aws_route53_record" "bahaanoah-com" {
+resource "aws_route53_record" "tonboazztek-com" {
   name    = var.domainName
   type    = "A"
   zone_id = aws_route53_zone.primary.zone_id
 
   alias {
-    name                   = aws_cloudfront_distribution.bahaanoah-com-s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.bahaanoah-com-s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.tonboazztek-com-s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.tonboazztek-com-s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 
 }
 
-resource "aws_route53_record" "www-bahaanoah-com" {
+resource "aws_route53_record" "www-tonboazztek-com" {
   name    = var.wwwDomainName
   type    = "A"
   zone_id = aws_route53_zone.primary.zone_id
 
   alias {
-    name                   = aws_cloudfront_distribution.www-bahaanoah-com-s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.www-bahaanoah-com-s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.www-tonboazztek-com-s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www-tonboazztek-com-s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
